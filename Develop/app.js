6bb8037558adb2f894e713b8713eedb7051bd136
Lisa Munoz <lisa_munoz817@yahoo.com>
2020-09-30 23:54:32 -0500

    added inquirer code

--- Develop/app.js
+++ Develop/app.js
@@ -9,6 +9,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
+const Choice = require("inquirer/lib/objects/choice");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -95,16 +97,51 @@ const managerQuest = [
     },
 ]
 
-function init() {
-    inquirer.prompt(employeeQuest).then(answers => {
-        const
-    })
-}
+
+    inquirer.prompt(roleQuest).then(choice => {
+        if (choice.Employee) {
+            inquirer.prompt(employeeQuest).then(answersemployeeQuest => {
+                const employee = new Employee(
+                    answersemployeeQuest.name,
+                    answersemployeeQuest.email,
+                    answersemployeeQuest.id
+                )
+            });
+        } else if (answer.Engineer) {
+            inquirer.prompt(engineerQuest).then(answersengineerQuest => {
+                const engineer = new Engineer(
+                    answersengineerQuest.name,
+                    answersengineerQuest.email,
+                    answersengineerQuest.id
+                )
+            });
+        } else if (answer.Intern) {
+            inquirer.prompt(internQuest).then(answersinternQuest => {
+                const intern = new Intern(
+                    answersinterneQuest.name,
+                    answersinternQuest.email,
+                    answersinternQuest.id
+                )
+            });
+        } else if (answer.Manager) {
+            inquirer.prompt(managerQuest).then(answersmanagerQuest => {
+                const manager = new Manager(
+                    answersmanagerQuest.name,
+                    answersmanagerQuest.email,
+                    answersmanagerQuest.id
+                )
+            });
+        }
+    });
 
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+// function render() {
+    
+// }
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
